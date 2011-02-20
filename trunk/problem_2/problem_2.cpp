ed48428ffda76b291e5a430e085e86423591fb25
ash.drone@gmail.com <ash.drone@gmail.com@251e6642-2893-be28-3c45-1609b35a4998>
2011-02-20 17:03:57 +0000

    Added solutions to problem 2 and problem 3

---
+++ trunk/problem_2/problem_2.cpp
@@ -0,0 +1,42 @@
+// problem_2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+double get_area(std::vector< double >& x, std::vector< double >& y)
+{
+   double or_area = 0.0;
+   
+   x.push_back(x[0]);
+   y.push_back(y[0]);
+   
+   for (int i = 0; i < x.size() - 1; i++)
+   {
+      or_area += x[i + 1] * y[i] - x[i] * y[i + 1];
+   }
+   
+   x.pop_back();
+   y.pop_back();
+   
+   return fabs(or_area * 0.5);
+}
+
+int main()
+{
+   std::ifstream input("in.txt");
+   std::ofstream output("out.txt");
+   
+   std::vector< double > x, y;
+   double cx, cy;
+   
+   while (input >> cx >> cy)
+   {
+      x.push_back(cx);
+      y.push_back(cy);
+   }
+   
+   double ans = get_area(x, y);
+   output << std::setprecision(6) << ans << std::endl;
+   
+   return 0;
+}
---
+++ trunk/problem_3/problem_3.cpp
@@ -0,0 +1,42 @@
+// problem_3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+int gcd_extended(int a, int b, int& x, int& y)
+{
+   if (a == 0)
+   {
+      x = 0; y = 1;
+      return b;
+   }
+   int x1, y1;
+   int d = gcd_extended(b % a, a, x1, y1);
+   x = y1 - (b / a) * x1;
+   y = x1;
+   
+   return d;
+}
+
+int main()
+{
+   std::ifstream input("in.txt");
+   std::ofstream output("out.txt");
+   
+   int A, B, C;
+   int x, y, g;
+   while (input >> A >> B >> C)
+   {
+      g = gcd_extended(A, B, x, y);
+      if (C % g != 0) output << "<none>" << std::endl;
+      else
+      {
+         x *= C / g;
+         y *= C / g;
+         output << x << ' ' << y << std::endl;
+      }
+   }
+   
+   return 0;
+}
+
