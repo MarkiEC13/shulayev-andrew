e32ccb7e25cc06c2857a095d153210024b01a2d7
ash.drone@gmail.com <ash.drone@gmail.com@251e6642-2893-be28-3c45-1609b35a4998>
2012-01-16 12:17:16 +0000

    500% faster than ever

--- trunk/cg2011/p1.1/geometry.cpp
+++ trunk/cg2011/p1.1/geometry.cpp
@@ -9,7 +9,7 @@ std::istream& operator>>(std::istream& in, point& p)
 	in >> p.x >> p.y;
 }
 
-std::ostream& operator<<(std::ostream& out, const point& p)
+std::ostream& operator<<(std::ostream& out, point const& p)
 {
 	out << '(' << p.x << "; " << p.y << ')';
 }
@@ -17,18 +17,18 @@ std::ostream& operator<<(std::ostream& out, const point& p)
 segment::segment() { }
 segment::segment(RandomInput& rand)
 {
-	end[0] = point(rand);
-	end[1] = point(rand);
+	a = point(rand);
+	b = point(rand);
 }
 
 std::istream& operator>>(std::istream& in, segment& s)
 {
-	in >> s.end[0] >> s.end[1];
+	in >> s.a >> s.b;
 }
 
-std::ostream& operator<<(std::ostream& out, const segment& s)
+std::ostream& operator<<(std::ostream& out, segment const& s)
 {
-	out << '(' << s.end[0] << ", " << s.end[1] << ')';
+	out << '(' << s.a << ", " << s.b << ')';
 }
 
 inline boost::numeric::interval<double> make_interval(double start, double end)
@@ -43,27 +43,27 @@ bool overlap(const boost::numeric::interval<double>& int1, const boost::numeric:
 	return !(cergt(int1, int2) || cerlt(int1, int2));
 }
 
-bool segments_intersect(const segment& segm1, const segment& segm2)
+bool segments_intersect(segment const& segm1, segment const& segm2)
 {
 	typedef boost::numeric::interval<double> interval;
 	
-	interval segm1_x = make_interval(segm1.end[0].x, segm1.end[1].x);
-	interval segm1_y = make_interval(segm1.end[0].y, segm1.end[1].y);
+	interval segm1_x = make_interval(segm1.a.x, segm1.b.x);
+	interval segm1_y = make_interval(segm1.a.y, segm1.b.y);
 	
-	interval segm2_x = make_interval(segm2.end[0].x, segm2.end[1].x);
-	interval segm2_y = make_interval(segm2.end[0].y, segm2.end[1].y);
+	interval segm2_x = make_interval(segm2.a.x, segm2.b.x);
+	interval segm2_y = make_interval(segm2.a.y, segm2.b.y);
 	
 	if (!overlap(segm1_x, segm2_x) || !overlap(segm1_y, segm2_y))
 		return false;
-	else if (left_turn(segm1, segm2.end[0]) * left_turn(segm1, segm2.end[1]) > 0)
+	else if (left_turn(segm1, segm2.a) * left_turn(segm1, segm2.b) > 0)
 		return false;
-	else if (left_turn(segm2, segm1.end[0]) * left_turn(segm2, segm1.end[1]) > 0)
+	else if (left_turn(segm2, segm1.a) * left_turn(segm2, segm1.b) > 0)
 		return false;
 	else
 		return true;
 }
 
-int left_turn(const segment& segm, const point& p)
+int left_turn(segment const& segm, point const& p)
 {
 	int result = left_turn_simple(segm, p);
 	if (result != 0)
@@ -80,12 +80,12 @@ int left_turn(const segment& segm, const point& p)
 	return left_turn_adaptive(segm, p);
 }
 
-int left_turn_simple(const segment& segm, const point& p)
+int left_turn_simple(segment const& segm, point const& p)
 {
 	double eps = 8 * std::numeric_limits<double>::epsilon() *
-		(std::abs((segm.end[1].x - segm.end[0].x) * (p.y - segm.end[0].y)) + std::abs((segm.end[1].y - segm.end[0].y) * (p.x - segm.end[0].x)));
+		(std::abs((segm.b.x - segm.a.x) * (p.y - segm.a.y)) + std::abs((segm.b.y - segm.a.y) * (p.x - segm.a.x)));
 	
-	double vec_prod = (segm.end[1].x - segm.end[0].x) * (p.y - segm.end[0].y) - (segm.end[1].y - segm.end[0].y) * (p.x - segm.end[0].x);
+	double vec_prod = (segm.b.x - segm.a.x) * (p.y - segm.a.y) - (segm.b.y - segm.a.y) * (p.x - segm.a.x);
 	if (vec_prod > eps)
 		return 1;
 	else if (vec_prod < -eps)
@@ -94,13 +94,13 @@ int left_turn_simple(const segment& segm, const point& p)
 		return 0;
 }
 
-int left_turn_interval(const segment& segm, const point& p)
+int left_turn_interval(segment const& segm, point const& p)
 {
 	typedef boost::numeric::interval<double> interval;
 	using boost::numeric::interval_lib::cergt;
 	using boost::numeric::interval_lib::cerlt;
 	
-	interval vector_prod = (interval(segm.end[1].x) - interval(segm.end[0].x)) * (interval(p.y) - interval(segm.end[0].y)) - (interval(segm.end[1].y) - interval(segm.end[0].y)) * (interval(p.x) - interval(segm.end[0].x));
+	interval vector_prod = (interval(segm.b.x) - interval(segm.a.x)) * (interval(p.y) - interval(segm.a.y)) - (interval(segm.b.y) - interval(segm.a.y)) * (interval(p.x) - interval(segm.a.x));
 	
 	if (cergt(vector_prod, 0.0))
 		return 1;
@@ -188,17 +188,17 @@ namespace adaptive_details
 
 };
 
-int left_turn_adaptive(const segment& segm, const point& p)
+int left_turn_adaptive(segment const& segm, point const& p)
 {
 	using namespace adaptive_details;
 	
 	double step1[12];
-	step1[1] = mul(segm.end[1].x, p.y, step1[0]);
-	step1[3] = mul(-segm.end[1].x, segm.end[0].y, step1[2]);
-	step1[5] = mul(-segm.end[0].x, p.y, step1[4]);
-	step1[7] = mul(-segm.end[1].y, p.x, step1[6]);
-	step1[9] = mul(segm.end[1].y, segm.end[0].x, step1[8]);
-	step1[11] = mul(segm.end[0].y, p.x, step1[10]);
+	step1[1] = mul(segm.b.x, p.y, step1[0]);
+	step1[3] = mul(-segm.b.x, segm.a.y, step1[2]);
+	step1[5] = mul(-segm.a.x, p.y, step1[4]);
+	step1[7] = mul(-segm.b.y, p.x, step1[6]);
+	step1[9] = mul(segm.b.y, segm.a.x, step1[8]);
+	step1[11] = mul(segm.a.y, p.x, step1[10]);
 	
 	double step2[12];
 	expand_sum_f<2, 2>::calc(step1 + 0, step1 + 2, step2);
