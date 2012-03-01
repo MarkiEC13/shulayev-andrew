5eba4ff1f40d1e8c340875fb0247210f8be32e92
ash.drone@gmail.com <ash.drone@gmail.com@251e6642-2893-be28-3c45-1609b35a4998>
2012-03-01 09:58:54 +0000

    изменён формат вывода, пока выпуклая оболочка не in-place

--- trunk/cg2012.1/01_convex_hull/main.cpp
+++ trunk/cg2012.1/01_convex_hull/main.cpp
@@ -10,29 +10,30 @@
 #include "shared/io.h"
 #include "shared/geometry.h"
 
-namespace comparator
+struct comparator
 {
-	std::pair<geometry::Point, int> start;
+	geometry::Point * start;
+
+	comparator(geometry::Point * start) : start(start)
+	{ }
 
 	int get_turn(const geometry::Point & p1, const geometry::Point & p2)
 	{
-		return geometry::left_turn(start.first, p1, p2);
+		return geometry::left_turn(*start, p1, p2);
 	}
 
-	bool less1(const std::pair<geometry::Point, int>& p1, const std::pair<geometry::Point, int>& p2)
+	bool operator()(const geometry::Point & p1, const geometry::Point & p2)
 	{
-		int turn = get_turn(p1.first, p2.first);
+		int turn = get_turn(p1, p2);
 		if (turn == 0)
 		{
-			// std::cerr << p1.first.get_x() << ';' << p1.first.get_y() << ' ';
-			// std::cerr << p2.first.get_x() << ';' << p2.first.get_y() << '\n';
-			if (p1.first.get_y() == p2.first.get_y())
+			if (p1.get_y() == p2.get_y())
 			{
-				return fabs(start.first.get_x() - p1.first.get_x()) < fabs(start.first.get_x() - p2.first.get_x());
+				return fabs(start->get_x() - p1.get_x()) < fabs(start->get_x() - p2.get_x());
 			}
 			else
 			{
-				return p1.first.get_y() < p2.first.get_y();
+				return p1.get_y() < p2.get_y();
 			}
 		}
 		else
@@ -42,88 +43,58 @@ namespace comparator
 	}
 };
 
+inline bool is_lower(const geometry::Point & p1, const geometry::Point & p2)
+{
+	return p1.get_y() > p2.get_y() || (p1.get_y() == p2.get_y() && p1.get_x() > p2.get_x());
+}
+
 int main()
 {
 	io::StreamInput in(std::cin);
 	int count = in.get_int();
 
-	if (count < 3)
-	{
-		std::cout << count << '\n';
-		for (int i = 0; i < count; ++i)
-		{
-			if (i > 0)
-			{
-				std::cout << ' ';
-			}
-			std::cout << (i + 1);
-		}
-		std::cout << '\n';
-	}
-
-	std::vector<std::pair<geometry::Point, int> > points(count);
+	std::vector<geometry::Point> points(count);
 	int selected = 0;
 	for (int i = 0; i < count; ++i)
 	{
-		points[i] = std::make_pair(geometry::Point(in), i);
-		if (i == 0 || comparator::start.first.get_y() > points[i].first.get_y()
-			|| (comparator::start.first.get_y() == points[i].first.get_y()
-				&& comparator::start.first.get_x() > points[i].first.get_x()))
+		points[i] = geometry::Point(in);
+		if (i == 0 || is_lower(points[selected], points[i]))
 		{
-			comparator::start = points[i];
 			selected = i;
 		}
 	}
 
-	// std::cerr << "start: " << comparator::start.first.get_x() << ';' << comparator::start.first.get_y() << '\n';
-	std::swap(points[selected], points[count - 1]);
-	points.pop_back();
+	std::swap(points[selected], points[0]);
+	std::sort(points.begin() + 1, points.end(), comparator(&points[0]));
 
-	std::sort(points.begin(), points.end(), &comparator::less1);
-
-	std::vector<std::pair<geometry::Point, int> > filtered_points;
-	for (int i = 0; i < count - 2; ++i) {
-		if (comparator::get_turn(points[i].first, points[i + 1].first) != 0)
-		{
-			filtered_points.push_back(points[i]);
-		}
-	}
-	filtered_points.push_back(points[count - 2]);
-
-	if (filtered_points.size() < 3)
+	if (points.size() < 3)
 	{
-		std::cout << filtered_points.size() + 1 << '\n' << comparator::start.second + 1;
-		for (size_t i = 0; i < filtered_points.size(); ++i)
+		std::cout << points.size() << '\n';
+		for (size_t i = 0; i < points.size(); ++i)
 		{
-			std::cout << ' ' << filtered_points[i].second + 1;
+			std::cout << points[i].get_x() << ' ' << points[i].get_y() << '\n';
 		}
-		std::cout << '\n';
 		return 0;
 	}
 
-	std::vector<std::pair<geometry::Point, int> > stack;
-	stack.push_back(comparator::start);
-	stack.push_back(filtered_points[0]);
-	stack.push_back(filtered_points[1]);
-	for (size_t i = 2; i < filtered_points.size(); ++i)
+	std::vector<geometry::Point> stack;
+	stack.push_back(points[0]);
+	stack.push_back(points[1]);
+	stack.push_back(points[2]);
+	for (size_t i = 3; i < points.size(); ++i)
 	{
-		while (stack.size() > 2 && geometry::left_turn(stack[stack.size() - 2].first, stack[stack.size() - 1].first, filtered_points[i].first) < 0)
+		while (stack.size() > 2 && geometry::left_turn(stack[stack.size() - 2], stack[stack.size() - 1], points[i]) != 1)
 		{
 			stack.pop_back();
 		}
-		stack.push_back(filtered_points[i]);
+		stack.push_back(points[i]);
 	}
 
 	std::cout << stack.size() << '\n';
 	for (size_t i = 0; i < stack.size(); ++i)
 	{		
-		if (i > 0)
-		{
-			std::cout << ' ';
-		}
-		std::cout << stack[i].second + 1;
+		std::cout << stack[i].get_x() << ' ' << stack[i].get_y() << '\n';
 	}
-	std::cout << '\n';
 
 	return 0;
 }
