54316c74f788057cafb3277c96a1843b98ff6f9b
ash.drone <ash.drone@gmail.com>
2012-03-03 21:29:49 +0000

    функция расчёта выпуклой оболочки вынесена из main.cpp

--- cg2012.1/01_convex_hull/main.cpp
+++ cg2012.1/01_convex_hull/main.cpp
@@ -54,42 +54,15 @@ int main()
 	int count = in.get_int();
 
 	std::vector<geometry::Point> points(count);
-	int selected = 0;
 	for (int i = 0; i < count; ++i)
 	{
 		points[i] = geometry::Point(in);
-		if (i == 0 || is_lower(points[selected], points[i]))
-		{
-			selected = i;
-		}
 	}
 
-	std::swap(points[selected], points[0]);
-	std::sort(points.begin() + 1, points.end(), comparator(&points[0]));
+	size_t size = geometry::convex_hull(points);
 
-	if (points.size() < 3)
-	{
-		std::cout << points.size() << '\n';
-		for (size_t i = 0; i < points.size(); ++i)
-		{
-			std::cout << points[i].get_x() << ' ' << points[i].get_y() << '\n';
-		}
-		return 0;
-	}
-
-	size_t stack_size = 3;
-	for (size_t i = 3; i < points.size(); ++i)
-	{
-		while (stack_size > 2 && geometry::left_turn(points[stack_size - 2], points[stack_size - 1], points[i]) != 1)
-		{
-			--stack_size;
-		}
-		std::swap(points[stack_size], points[i]);
-		++stack_size;
-	}
-
-	std::cout << stack_size << '\n';
-	for (size_t i = 0; i < stack_size; ++i)
+	std::cout << size << '\n';
+	for (size_t i = 0; i < size; ++i)
 	{		
 		std::cout << points[i].get_x() << ' ' << points[i].get_y() << '\n';
 	}
